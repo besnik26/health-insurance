92c77451e36937915ef43dbaf287d8d3ab7cd709
besnik <besnikbeka2001@gmail.com>
2024-07-18 17:45:46 +0200

    Home page update

---
+++ src/app/global-form/global-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-form.component.html',
+  styleUrl: './global-form.component.scss'
+})
+export class GlobalFormComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GlobalFormComponent } from '../global-form/global-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [GlobalFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

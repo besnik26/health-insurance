7cbef81fccf3b2a3f6b4e483a3f72366de56ea4a
besnik <besnikbeka2001@gmail.com>
2024-07-19 11:49:14 +0200

    Global form update

--- src/app/global-form/global-form.component.ts
+++ src/app/global-form/global-form.component.ts
@@ -1,12 +1,39 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-global-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgClass,
+    NgStyle,
+    RouterLink
+  ],
   templateUrl: './global-form.component.html',
   styleUrl: './global-form.component.scss'
 })
 export class GlobalFormComponent {
+
+  myForm!:FormGroup;
+  constructor(private formBuilder: FormBuilder,){
+    this.buildForm();
+  }
+
+
+  private buildForm(){
+    const emailPattern = /^[-!#$%&'*+/=?^_`{|}~A-Za-z0-9]+(?:\.[-!#$%&'*+/=?^_`{|}~A-Za-z0-9]+)*@([A-Za-z0-9]([A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9]$/;
+    this.myForm = this.formBuilder.group({
+      
+    })
+  }
 
 }

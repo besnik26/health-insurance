e8334b6f7d2357009bdbb5996632f710a0b400cc
besnik <besnikbeka2001@gmail.com>
2024-08-01 12:39:44 +0200

    minor change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { LangChangeEvent } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Component } from '@angular/core';
 import { GlobalFormComponent } from '../global-form/global-form.component';
 import { RouterLink } from '@angular/router';
@@ -10,5 +12,13 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+selectedLanguage:string = 'en';
 
+  constructor(private translateService: TranslateService){
+    this.selectedLanguage = this.translateService.currentLang || 'en';
+    this.translateService.onLangChange.subscribe((event:LangChangeEvent)=>{
+      this.selectedLanguage =event.lang;
+    })
+  
+  }
 }
